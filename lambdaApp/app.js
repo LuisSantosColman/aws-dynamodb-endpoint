edb066972e3f5a6afb3b623914b3be1473c7aa4a
Luis Santos Colman <28844297+LuisSantosColman@users.noreply.github.com>
2023-12-13 20:03:57 +0100

    Update app.js

--- lambdaApp/app.js
+++ lambdaApp/app.js
@@ -1,32 +1,34 @@
 const AWS = require('aws-sdk');
 
 exports.handler = async (event) => {
-    const { id, newData } = JSON.parse(event.body);
+    //const { id, newData } = JSON.parse(event.body);
+    const apiPayload = JSON.parse(event.body);
 
     const dynamoDB = new AWS.DynamoDB.DocumentClient();
     const tableName = process.env.TABLE_NAME;
 
     const params = {
         TableName: tableName,
-        Key: { id: id },
-        UpdateExpression: 'set newData = :newData',
+        Key: { id: apiPayload.id },
+        UpdateExpression: 'set hasAcceptedTerms = :hasAcceptedTerms',
         ExpressionAttributeValues: {
-            ':newData': newData,
+            ':hasAcceptedTerms': 'yes'
         },
-        ReturnValues: 'UPDATED_NEW',
+        ReturnValues: 'UPDATED_NEW'
     };
 
     try {
         const result = await dynamoDB.update(params).promise();
         return {
             statusCode: 200,
-            body: JSON.stringify(result.Attributes),
+            //body: JSON.stringify(result.Attributes)
+            body: 'success'
         };
     } catch (error) {
         console.error('Error updating item:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' }),
+            body: JSON.stringify({ error: error })
         };
     }
 };
