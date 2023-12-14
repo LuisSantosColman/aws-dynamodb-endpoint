22ba3f4b8626255d5d2ea2f181b08f6f81b3fb1d
Luis Santos Colman <28844297+LuisSantosColman@users.noreply.github.com>
2023-12-14 11:46:39 +0100

    Create app.js

---
+++ readApp/app.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event) => {
+    
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+    const tableName = process.env.TABLE_NAME;
+    var userId = event.queryStringParameters.u;
+
+    const params = {
+        TableName: tableName,
+        Key: {
+            // Define your key here
+            // For example, if your partition key is 'id'
+            id: userId
+        }
+    };
+
+    try {
+        const data = await dynamoDB.get(params).promise();
+
+        return {
+            statusCode: 200,
+            //body: JSON.stringify(data.Item)
+            body: 'OK'
+        };
+    } catch (error) {
+        console.error('Error querying DynamoDB:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' })
+        };
+    }
+};
