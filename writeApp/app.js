ea940d170843360f492612fa9de5e08e8f40438d
Luis Santos Colman <28844297+LuisSantosColman@users.noreply.github.com>
2023-12-14 13:41:29 +0100

    Update app.js

--- writeApp/app.js
+++ writeApp/app.js
@@ -4,10 +4,32 @@ exports.handler = async (event) => {
     const dynamoDB = new AWS.DynamoDB.DocumentClient();
     const tableName = process.env.TABLE_NAME;
     const apiPayload = JSON.parse(event.body);
+    const onlyLettersAndNumbers = function(str) {
+    	return /^[A-Za-z0-9]*$/.test(str);
+    }
+    
+    if (apiPayload.hasOwnProperty('id')) {
+        const userId = apiPayload.id.toString();
+        const isValidUserId = onlyLettersAndNumbers(userId);
+        if (!isValidUserId || !userId) {
+            /* Returning a 500 error if the value of the "id" parameter posted contains characters other than a string with only letters and numbers */
+            return {
+                statusCode: 500,
+                body: 'Internal Server Error'
+            };
+        }
+    }
+    else {
+        /* Returning a 500 error if the "id" parameter was not passed in the posted payload */
+        return {
+            statusCode: 500,
+            body: 'Internal Server Error'
+        };
+    }
 
     const params = {
         TableName: tableName,
-        Key: { id: apiPayload.id },
+        Key: { id: userId },
         UpdateExpression: 'set hasAcceptedTerms = :hasAcceptedTerms',
         ExpressionAttributeValues: {
             ':hasAcceptedTerms': 'yes'
