8d22825037c216a499c033fe585be1a5c661b192
Luis Santos Colman <28844297+LuisSantosColman@users.noreply.github.com>
2023-12-14 20:39:03 +0100

    Update app.js

--- writeApp/app.js
+++ writeApp/app.js
@@ -1,58 +1,111 @@
-/* Example of expected payload for the POST request: { "id": "Miro-User-ID" } */
-
 const AWS = require('aws-sdk');
 
 exports.handler = async (event) => {
-    const dynamoDB = new AWS.DynamoDB.DocumentClient();
-    const tableName = process.env.TABLE_NAME;
-    const apiPayload = JSON.parse(event.body);
+    
     const onlyLettersAndNumbers = function(str) {
     	return /^[A-Za-z0-9]*$/.test(str);
     }
     
-    if (apiPayload.hasOwnProperty('id')) {
-        const userId = apiPayload.id.toString();
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+    const tableName = process.env.TABLE_NAME;
+    
+    if (event && event.hasOwnProperty('queryStringParameters') && !event.queryStringParameters) {
+        
+        /* Returning a 500 error if the URL parameter "u" is not passed in the GET request */
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                "Access-Control-Allow-Methods": "GET" // Allow only GET request 
+            },
+            body: 'Internal Server Error'
+        };
+    }
+    
+    if (event && event.hasOwnProperty('queryStringParameters') && event.queryStringParameters.hasOwnProperty('u') && event.queryStringParameters.u !== '') {
+        
+        const userId = event.queryStringParameters.u.toString();
         const isValidUserId = onlyLettersAndNumbers(userId);
-        if (!isValidUserId || !userId) {
-            /* Returning a 500 error if the value of the "id" parameter posted contains characters other than a string with only letters and numbers */
+        
+        if (!isValidUserId) {
+            
+            /* Returning a 500 error if the value of the URL parameter "u" passed in the GET request contains characters other than letters and numbers */
             return {
                 statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                    "Access-Control-Allow-Methods": "GET" // Allow only GET request 
+                },
                 body: 'Internal Server Error'
             };
         }
     }
     else {
-        /* Returning a 500 error if the "id" parameter was not passed in the posted payload */
         return {
             statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                "Access-Control-Allow-Methods": "GET" // Allow only GET request 
+            },
             body: 'Internal Server Error'
         };
     }
+    
+    const userId = event.queryStringParameters.u.toString();
 
     const params = {
         TableName: tableName,
-        Key: { id: userId },
-        UpdateExpression: 'set hasAcceptedTerms = :hasAcceptedTerms',
-        ExpressionAttributeValues: {
-            ':hasAcceptedTerms': 'yes'
-        },
-        ReturnValues: 'UPDATED_NEW'
+        Key: {
+            id: userId
+        }
     };
 
     try {
-        const result = await dynamoDB.update(params).promise();
-        return {
-            statusCode: 200,
-            //body: JSON.stringify(result.Attributes)
-            body: 'OK'
-        };
-    } catch (error) {
-        console.error('Error updating item:', error);
+        const data = await dynamoDB.get(params).promise();
+        
+        if (data && data.hasOwnProperty('Item') && data.Item.hasOwnProperty('id') && data.Item.id.toString() === userId) {
+            
+            /* Returning a 200 OK if the value of the URL parameter "u" matches an entry in the MiroBannerTermsAccepters DynamoDB table */
+            return {
+                statusCode: 200,
+                //body: JSON.stringify(data.Item)
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                    "Access-Control-Allow-Methods": "GET" // Allow only GET request 
+                },
+                body: 'OK'
+            };
+        }
+        else {
+            
+            /* Returning a 404 error if the value of the URL parameter "u" does not match any entry in the DynamoDB table */
+            return {
+                statusCode: 404,
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                    "Access-Control-Allow-Methods": "GET" // Allow only GET request 
+                },
+                body: 'Not Found'
+            };
+        }
+    } 
+    catch (error) {
+        console.error('Error querying DynamoDB:', error);
         
         /* Returning a 500 error for any other error not catched above */
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' })
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                "Access-Control-Allow-Methods": "GET" // Allow only GET request 
+            },
+            body: 'Internal Server Error'
         };
     }
 };
