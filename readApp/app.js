8426e38b9f48dd9f7e2f8ad9e70995c9850c0176
Luis Santos Colman <28844297+LuisSantosColman@users.noreply.github.com>
2023-12-14 12:40:06 +0100

    Update app.js

--- readApp/app.js
+++ readApp/app.js
@@ -4,31 +4,38 @@ exports.handler = async (event) => {
     
     const dynamoDB = new AWS.DynamoDB.DocumentClient();
     const tableName = process.env.TABLE_NAME;
-    var userId = event.queryStringParameters.u;
+    var userId = event.queryStringParameters.u.toString();
 
     const params = {
         TableName: tableName,
         Key: {
-            // Define your key here
-            // For example, if your partition key is 'id'
             id: userId
         }
     };
 
     try {
         const data = await dynamoDB.get(params).promise();
-
-        return {
-            statusCode: 200,
-            //body: JSON.stringify(data.Item)
-            body: 'OK'
-        };
-    } catch (error) {
+        
+        if (data && data.hasOwnProperty('Item') && data.Item.hasOwnProperty('id') && data.Item.id.toString() === userId) {
+            return {
+                statusCode: 200,
+                //body: JSON.stringify(data.Item)
+                body: 'OK'
+            };
+        }
+        else {
+            return {
+                statusCode: 404,
+                body: 'Not Found'
+            };
+        }
+    } 
+    catch (error) {
         console.error('Error querying DynamoDB:', error);
 
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' })
+            body: 'Internal Server Error'
         };
     }
 };
