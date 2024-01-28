e4cd4312a86cc596404f59c92c6ae8508eef1b44
Luis Santos Colman <28844297+LuisSantosColman@users.noreply.github.com>
2024-01-28 15:59:47 +0100

    Update app.js

--- readApp/app.js
+++ readApp/app.js
@@ -10,8 +10,8 @@ exports.handler = async (event) => {
     };
     
     const responseHeaders = {
-        "Access-Control-Allow-Headers" : "Accept",
-        /*"Access-Control-Allow-Origin": "*", // Allow from anywhere*/
+        "Access-Control-Allow-Headers" : "Content-Type,Accept,Cache-Control",
+        "Access-Control-Allow-Origin": "*", // Allow from anywhere
         "Access-Control-Allow-Methods": "GET", // Allow only GET request
         "Content-Type": "application/json"
     };
